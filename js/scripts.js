2a3e7cadb68f2d7ed8bb41ec1f0627f37d1d1686
ChristineMulindi <mulindichristine@gmail.com>
2019-03-24 19:26:20 +0300

    Correct spelling mistake

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
-function triangleTracker() {
-  var length1 = [parseFloat(prompt('Input length1'))]
-  var length2 = [parseFloat(prompt('Input length2'))]
-  var length3 = [parseFloat(prompt('Input length3'))]
+function triangle() {
+  var length1 = document.getElementById('length1').value;
+  var length2 = document.getElementById('length2').value;
+  var length3 = document.getElementById('length3').value;
 
-  var ab = length1.concat(length2);
+  var side1 = [parseFloat(length1)];
+  var side2 = [parseFloat(length2)];
+  var side3 = [parseFloat(length3)];
 
-  var abc = ab.concat(length3);
+  var output = document.getElementById('output');
+
+  var ab = side1.concat(side2);
+
+  var abc = ab.concat(side3);
 
   var sorted = abc.sort(function(d, e) {
     return d - e;
-  })
+  });
 
-  var a = sorted[0]
-  var b = sorted[1]
-  var c = sorted[2]
+  var a = sorted[0];
+  var b = sorted[1];
+  var c = sorted[2];
 
   var lengthab = a + b;
 
 
   if (lengthab < c) {
-    alert("ERROR! NOT A TRIANGLE")
+    output.textContent = "ERROR! NOT A TRIANGLE";
   } else if (a === b && b === c && a > 0 && b > 0 && c > 0) {
-    console.log('Equilateral Triangle')
-    alert('EQUILATERAL TRIANGLE')
+    output.textContent = 'EQUILATERAL TRIANGLE';
   } else if (a === b && b !== c || a !== b && b === c && a > 0 && b > 0 && c > 0) {
-    console.log('Isosceles Triangle')
-    alert('ISOCELES TRIANGLE')
+    output.textContent = ('ISOCELES TRIANGLE');
   } else if (a !== b && b !== c && (Number.isInteger(a) === true || Number.isInteger(b) === true || Number.isInteger(c) === true) && a > 0 && b > 0 && c > 0) {
-    console.log('Scalene Triangle')
-    alert('SCALENE TRIANGLE')
+    output.textContent = 'SCALENE TRIANGLE';
   } else {
-    alert("INVALID MEASUREMENTS!!")
+    output.textContent = "INVALID MEASUREMENTS!!";
   };
 }
